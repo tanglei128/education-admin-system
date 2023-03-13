542b01dd5d06123d3b7f021f959b04ca402d94c5
tanglei <tanglei.cosmo@haier.com>
2023-03-13 21:27:58 +0800

    fix:解决date日期入参传值问题

--- src/main/java/com/tyy/educationadminsystem/entity/User.java
+++ src/main/java/com/tyy/educationadminsystem/entity/User.java
@@ -2,7 +2,9 @@ package com.tyy.educationadminsystem.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -16,6 +18,8 @@ public class User extends BaseEntity implements Serializable{
     private String realName;
     private Integer age;
     private Integer sex;//0:女，1：男
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+08:00")
     private Date birthday;
     private String phone;
     private Integer classNum;//年级号
--- src/main/java/com/tyy/educationadminsystem/vo/UserVo.java
+++ src/main/java/com/tyy/educationadminsystem/vo/UserVo.java
@@ -1,11 +1,14 @@
 package com.tyy.educationadminsystem.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.io.Serializable;
 import java.util.Date;
 @Data
-public class UserVo{
+public class UserVo implements Serializable {
     private String id;
     @ApiModelProperty(value = "用户名", required = true)
     private String name;
@@ -14,7 +17,9 @@ public class UserVo{
     private String realName;
     private Integer age;
     private Integer sex;//0:女，1：男
-    private String birthday;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+08:00")
+    private Date birthday;
     private String phone;
     private Integer classNum;//年级号
     private Integer status;//状态0：禁用，1启用
